899c70ee2d14b9239a80caf0e22c23fe35d4d4b7
OliaVasileva <onjel2295@gmail.com>
2022-06-16 20:15:32 +0300

    Тест 2.1

--- cypress/e2e/projects/const.js
+++ cypress/e2e/projects/const.js
@@ -1,3 +1,10 @@
 export class Constant {
     static urlPage = 'https://www.tinkoff.ru/';
+    static loansLandingPage = 'https://business-qa.tcsbank.ru/loans/landing';
+    // static authorizePage = 'https://id-qa.tcsbank.ru/auth/step?cid=W3vc2ulxpvLU';
+
+    static UserWithoutLoans = {
+        number : '79374351899',
+        smsCode : '1234'
+    }
 }
